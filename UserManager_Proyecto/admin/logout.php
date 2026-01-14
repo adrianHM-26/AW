78bb99a20c2fdf017b577078b21e1aa2d04ae656
adrian <adrian1234567t@gmail.com>
2026-01-14 19:30:27 +0100

    Sexta modificacion

--- UserManager_Proyecto/admin/logout.php
+++ UserManager_Proyecto/admin/logout.php
@@ -12,15 +12,6 @@ if (!isset($_SESSION['user_id'])) {
 // Destruir sesión completamente
 $_SESSION = array();
 
-// Si se usa cookie de sesión, borrarla
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-
 // Destruir sesión
 session_destroy();
 
